1272a72efb0b9a221e889b22ced8bee8d3eeb99d
Louis-Gabriel Dournois <ldournoi@student.42angouleme.fr>
2022-04-28 22:13:04 +0200

    :crane:

---
+++ cfr.h
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cfr.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldournoi <ldournoi@student.42angouleme.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 21:43:42 by ldournoi          #+#    #+#             */
+/*   Updated: 2022/04/28 22:09:50 by ldournoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CFR_H
+# define CFR_H
+
+# define TantQue while
+# define Pour for
+# define Si if
+# define Sinon else
+# define Sauter continue
+# define Faire do
+# define FaireTantQue do while
+# define Retourner return
+# define neant void
+# define entier int
+# define accoladegauche {
+# define accoladedroite }
+# define reel double
+# define caractere char
+# define chainedechar char*
+# define chainedeint int*
+# define chainedereel double*
+# define tableaudechar char**
+# define tableaudeint int**
+# define pointeur *
+# define pointeurdepointeur **
+# define pointeurdepointeurdepointeur ***
+# define pointeurdepointeurdepointeurdepointeur ****
+# define pointeurdepointeurdepointeurdepointeurdepointeur *****
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeur ******
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur *******
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur ********
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur *********
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur **********
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur ***********
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur ************
+# define pointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeurdepointeur *************
+# define pointvirgule ;
+# define inferieur <
+# define superieur >
+# define inferieuregal <=
+# define superieuregal >=
+# define egal ==
+# define different !=
+# define et &&
+# define ou ||
+# define decalerbitsdroite >>
+# define decalerbitsgauche <<
+# define allouememoire(x) malloc(x)
+# define liberememoire(x) free(x)
+# define taille(x) sizeof(x)
+# define principal(x) main(x)
+# define definirtype typedef
+# define definirconstante const
+# define varglobale extern
+# define attribuer =
+# define ecrire(x, y, z) write(x, y, z)
+#endif
---
+++ test.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldournoi <ldournoi@student.42angouleme.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 22:02:29 by ldournoi          #+#    #+#             */
+/*   Updated: 2022/04/28 22:08:53 by ldournoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cfr.h"
+#include <unistd.h>
+
+entier principal(neant) accoladegauche
+	chainedechar test pointvirgule
+
+	test attribuer "coucou\n" pointvirgule
+	ecrire(1, test, 7) pointvirgule
+
+accoladedroite
